c3f9a9d562879996ef0073904cabbfacfdfa1376
quangvt <vutrongquang@gmail.com>
2016-12-26 02:59:24 +0700

    End chapter 3: Using Bootstrap components

---
+++ QuickShipWeb/Hubs/ProgressbarHub.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using Microsoft.AspNet.SignalR;
+
+namespace QuickShipWeb.Hubs
+{
+    public class ProgressbarHub :Hub
+    {
+        public void SendProgress()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Thread.Sleep(50);
+                Clients.Caller.sendMessage(i + "%");
+            }
+        }
+    }
+}
