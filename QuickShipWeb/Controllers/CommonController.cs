eda845e07c4153f53d3400b1604e73d154f7c2dc
quangvt <vutrongquang@gmail.com>
2016-12-27 23:28:44 +0700

    Chap 4: end

---
+++ QuickShipWeb/Controllers/CommonController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuickShipWeb.Controllers
+{
+    public class CommonController : Controller
+    {
+        // GET: Common
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Accordion()
+        {
+            return View();
+        }
+
+        public ActionResult Carousel()
+        {
+            return View();
+        }
+    }
+}
